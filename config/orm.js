e47e3e701683b06839d1034f836b77f974cba626
Brittani <brittani1128@gmail.com>
2018-09-05 08:40:52 -0500

    set up folders and files

---
+++ config/orm.js
@@ -0,0 +1,40 @@
+var connection = require(".../config/connection.js");
+
+
+
+
+var orm = {
+
+    selectAll: function(table, cb){
+        var queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err
+            };
+            cb(result);
+        });
+        
+    },
+    insertOne: function(table, cols, vals, cb){
+        var queryString = "INSERT INTO " + table + "(" + cols.toString() + ") VALUES (" + vals + ");" 
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+
+    },
+    //Need to ADD HELPER FUNCTION to convert ObjColVals to string
+    updateOne: function(table, objColVals, condition, cb){
+        var queryString = "UPDATE " + table + " SET " + objColVals + " WHERE " + condition + ";"
+        connection.query(queryString, objColVals, function(err, result){
+            if (err) {
+                throw err
+            }
+            cb(result);
+        }); 
+    }
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+###write insert queries to populate the burgers table with at least three entries.
+
+INSERT INTO burgers (burger_name) VALUES ("BBQ Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Bison Burger");
+INSERT INTO burgers (burger_name) VALUES ("Black and Bleu Burger");
+
