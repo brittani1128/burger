f83d1fd7bcc3a9670c295ac69649b0a9e07f97bd
Brittani <brittani1128@gmail.com>
2018-09-06 20:14:16 -0500

    added delete

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@ INSERT INTO burgers (name) VALUES ("BBQ Bacon Cheeseburger");
 INSERT INTO burgers (name) VALUES ("Black and Bleu Burger");
 INSERT INTO burgers (name) VALUES ("Turkey Avocado Burger");
 INSERT INTO burgers (name) VALUES ("Bison Burger");
-INSERT INTO burgers (name) VALUES ("Classic Cheeseburger");
+INSERT INTO burgers (name) VALUES ("Double Cheeseburger");
