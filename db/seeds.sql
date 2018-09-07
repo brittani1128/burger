acaec83143b6122d0274942cd56df8ede202d5b9
Brittani <brittani1128@gmail.com>
2018-09-06 20:54:22 -0500

    update readme

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (name) VALUES ("BBQ Bacon Cheeseburger");
-INSERT INTO burgers (name) VALUES ("Black and Bleu Burger");
-INSERT INTO burgers (name) VALUES ("Turkey Avocado Burger");
-INSERT INTO burgers (name) VALUES ("Bison Burger");
-INSERT INTO burgers (name) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (name) VALUES ("Hit Me With Your Best Shallot Burger");
+INSERT INTO burgers (name) VALUES ("Thank God It's Fried Egg Burger");
+INSERT INTO burgers (name) VALUES ("Foot Feta-ish Burger");
+INSERT INTO burgers (name) VALUES ("New Bacon-ings Burger");
+INSERT INTO burgers (name) VALUES ("The Tunami Burger");
